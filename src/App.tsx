c6c4df4df9c1197c91f7c87767b4488273516485
Albert <electron826@gmail.com>
2021-03-31 23:47:41 +0200

    Generalized view so that it can be reused.

--- src/App.tsx
+++ src/App.tsx
@@ -50,70 +50,127 @@ function date_from_tuple (dtup: number[]) {
     return new Date(dtup[0], dtup[1] - 1, dtup[2]);
 }
 
+/* General */
+
+interface TileState {
+    labels: string[]
+}
+
+interface TileProps extends React.HTMLAttributes<unknown> {
+    offset: number
+    labels?: string[]
+}
+
+abstract class Tile<P extends TileProps = TileProps, S extends TileState = TileState> extends React.Component<P, S> {
+    constructor (props: P) {
+        super(props);
+
+        this.state = {
+            labels: props.labels ?? []
+        } as S;     // https://stackoverflow.com/questions/51651678/react-with-typescript-type-is-not-assignable-to-type-readonlys
+    }
+
+    abstract get_progress(): number | null;
+
+    get_indicator(): JSX.Element | null {
+        var day_frac = this.get_progress();
+
+        if (day_frac != null) {
+            return (
+                <div className="indicator" style={{width: (day_frac * 100) + '%'}}></div>
+            );
+        } else {
+            return null;
+        }
+    }
+
+    abstract render_tile(): React.ReactNode;
+
+    render() {
+        return (
+            <div className="tile-container" style={this.props.style}>
+                {this.state.labels.map(label => (<div className="day-label">{label}</div>))}
+                {this.render_tile()}
+            </div>
+        );
+    }
+}
+
+abstract class TileStrip<P, S> extends React.Component<P, S> {
+}
+
 /* Day */
 
-interface DayState {
-    date: Date
+interface DayState extends TileState {
+    date: Date,
 }
 
-interface DayProps extends React.HTMLAttributes<unknown> {
-    offset: number
+interface DayProps extends TileProps {
+    offset: number,
 }
 //18717
-class Day extends React.Component<DayProps, DayState> {
+class Day extends Tile<DayProps, DayState> {
 	constructor(props: DayProps) {
 		super(props);
 
         this.state = {
-            date: new Date((1000*60*60*24)*props.offset)
+            date: new Date((1000*60*60*24)*props.offset),
+            ...(this.state as TileState),
         };
+
+        if (this.state.date.getDate() == 1) {
+            this.state.labels.push(Day.month_names_full[this.state.date.getMonth()]);
+        }
 	}
 
-    get_indicator(): JSX.Element {
-      var now = new Date();//this.state.date;
-      var day_fract = (60 * now.getHours() + now.getMinutes()) / (60 * 24);
-
-      return (
-        <div className="indicator" style={{width: (day_fract * 100) + '%'}}></div>
-      );
+    get_progress() {
+        if (Day.since_epoch(this.state.date) == Day.since_epoch(new Date)) {
+            var now = new Date();
+            return (60 * now.getHours() + now.getMinutes()) / (60 * 24);
+        } else {
+            return null;
+        }
     }
 
-	render() {
-        var day_no = this.state.date.getDay();  // Day of the week
-
-        var class_name = 'day-tile';
-        if  (day_no == 0 || day_no == 6)  { class_name += ' day-weekend'; }
-
-        var label = <div className="day-label">Today!</div>
+    render_tile() {
+        var class_name = 'tile day-tile';
+        if (this.is_weekend()) {
+            class_name += ' day-weekend';
+        }
 
         return (
-          <div className={class_name} style={this.props.style}>
-            {Day.is_holiday(this.state.date) ? <div className="day-holiday"/> : null}
-            <h2 className="day-no">{this.state.date.getDate()}</h2>
-            <h3 className="day-name">{Day.names[day_no]}</h3>
-            {this.props.offset == Day.since_epoch(new Date()) ? this.get_indicator() : null}
-            {this.props.offset == Day.since_epoch(new Date()) ? label : null}
-          </div>
+            <div className={class_name}>
+                {this.is_holiday() ? <div className="day-holiday"/> : null}
+                <h2 className="day-no">{this.state.date.getDate()}</h2>
+                <h3 className="day-name">{Day.names[this.state.date.getDay()]}</h3>
+                {this.get_indicator()}
+                {this.props.offset == Day.since_epoch(new Date()) ? this.get_indicator() : null}
+            </div>
         );
-	}
+    }
 
     /* Real World Stuff */
 
     static names: string[] = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    static month_names_full: string[] = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-    static is_holiday(date: Date) {
+    is_holiday(): boolean {
         for (var i = 0; i < HOLIDAYS.length; i++) {
             var start = date_from_tuple(HOLIDAYS[i].start);
             var end   = date_from_tuple(HOLIDAYS[i].end);
 
-            if (start.getTime() <= date.getTime() && date.getTime() < end.getTime())
+            if (start.getTime() <= this.state.date.getTime() && this.state.date.getTime() < end.getTime())
             {
-                console.log(date.toString() + "is a holiday")
                 return true;
             }
         }
 
         return false;
+    }
+
+    is_weekend(): boolean {
+        var day_no = this.state.date.getDay();  // Day of the week
+        return day_no == 0 || day_no == 6;
     }
 
     static since_epoch(date: Date): number      // Days since the epoch
@@ -123,35 +180,116 @@ class Day extends React.Component<DayProps, DayState> {
     }
 }
 
-class DayView extends FixedSizeList
+interface DayStripProps {
+}
+
+class DayStrip extends TileStrip<DayStripProps, {}>
 {
-    constructor(props: FixedSizeListProps) {
+    constructor(props: DayStripProps) {
         super(props);
     }
 
-    componentDidMount() {
-        var idx = Day.since_epoch(new Date);
-        this.scrollToItem(idx, "center");
+    render() {
+        var tile_style = get_css_class(".tile").style;
+        var day_style = get_css_class(".day-tile").style;
+
+        // height={2*parseInt(day_style.borderWidth) + parseInt(day_style.height) + parseInt(day_style.marginTop) + parseInt(day_style.marginBottom)}
+        return (
+            <FixedSizeList
+                ref={view => {if (view != null) {(view as FixedSizeList).scrollToItem(Day.since_epoch(new Date), "center"); }}}
+                className="tile-strip"
+                height={200}
+                width={800}
+                itemCount={45000}
+                itemSize={parseInt(tile_style.borderWidth) + parseInt(day_style.width)}
+                layout="horizontal"
+            >
+                {({index, style}) => { return (
+                    <Day offset={index} style={{position: style.position, left: style.left}}/>
+                );}}
+            </FixedSizeList>
+        );
     }
 }
 
-function App() {
-    var day_style = get_css_class(".day-tile")?.style;
-    var day_view: React.RefObject<DayView> = React.createRef();
+/* Decade */
 
-    return (
-        <div className="App">
-            <DayView
-                ref={day_view}
-                className="days-strip"
-                height={2*parseInt(day_style.borderWidth) + parseInt(day_style.height) + parseInt(day_style.marginTop) + parseInt(day_style.marginBottom)}
-                width={700}
-                itemCount={45000}
-                itemSize={parseInt(day_style.borderWidth) + parseInt(day_style.width)}
+interface DecadeProps extends TileProps {
+    offset: number,
+}
+
+interface DecadeState extends TileState {
+    first_year: number,
+}
+
+class Decade extends Tile<DecadeProps, DecadeState> {
+	constructor(props: DecadeProps) {
+		super(props);
+
+        this.state = {
+            first_year: DecadeStrip.first + props.offset*10,
+            ...(this.state as TileState),
+        };
+	}
+
+    get_progress() {
+        if (Math.floor((new Date).getFullYear() / 10) == this.state.first_year / 10) {
+            var now = new Date();
+            return (now.getFullYear() - this.state.first_year) / 10;
+        } else {
+            return null;
+        }
+    }
+
+    render_tile() {
+        return (
+            <div className="tile decade-tile">
+                <h2 className="day-no">{this.state.first_year}</h2>
+                {this.get_indicator()}
+            </div>
+        );
+    }
+}
+
+class DecadeStrip extends TileStrip<{}, {}>
+{
+    static first = 1700;
+    static last  = 2120;
+
+    constructor(props: DayStripProps) {
+        super(props);
+    }
+
+    render() {
+        var tile_style = get_css_class(".tile").style;
+        var decade_style = get_css_class(".decade-tile").style;
+
+        // height={2*parseInt(day_style.borderWidth) + parseInt(day_style.height) + parseInt(day_style.marginTop) + parseInt(day_style.marginBottom)}
+        return (
+            <FixedSizeList
+                ref={view => {if (view != null) {(view as FixedSizeList).scrollToItem(((new Date).getFullYear() - DecadeStrip.first) / 10, "center"); }}}
+                className="tile-strip"
+                height={200}
+                width={800}
+                itemCount={(DecadeStrip.last - DecadeStrip.first) / 10}
+                itemSize={parseInt(tile_style.borderWidth) + parseInt(decade_style.width)}
                 layout="horizontal"
             >
-                {({index, style}) => { return (<div><Day offset={index} style={{position: style.position, left: style.left}}/></div>);}}
-            </DayView>
+                {({index, style}) => { return (
+                    <Decade offset={index} style={{position: style.position, left: style.left}}/>
+                );}}
+            </FixedSizeList>
+        );
+    }
+}
+
+/* App */
+
+function App() {
+    return (
+        <div className="App">
+            <DayStrip/>
+            <DecadeStrip/>
         </div>
     );
 }
